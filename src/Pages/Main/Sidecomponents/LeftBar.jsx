10516b51bcc4f924357d70f0daf6cd227bb1da82
Kenneth Aduan <kenneth.aduan10@gmail.com>
2023-01-03 06:55:18 +0800

    main changes

--- src/Pages/Main/Sidecomponents/LeftBar.jsx
+++ src/Pages/Main/Sidecomponents/LeftBar.jsx
@@ -5,41 +5,50 @@ import Connections from "../../../Assets/images/Connections.png";
 import Events from "../../../Assets/images/Events.png";
 import Groups from "../../../Assets/images/Groups.png";
 
-const LeftBar = ({ sizing }) => {
+const LeftBar = () => {
   return (
-    <div className={sizing}>
-      <div className="leftbar">
-        <div className="rightContainer w-96 bg-white mt-2 ml-4 border rounded">
-          <div className="bg-gray border h-16 text-center text-sm bg-slate-400">
-            <p className="mt-4">BACKGROUND TO COVER PHOTO</p>
+    <div>
+      <div className="leftbar w-60 mt-2 ml-2 border rounded">
+        <div className="rightContainer">
+          <div className="bg-gray-300 border pb-2 text-center text-sm">
+            <p className="mt-4 text-sm">BACKGROUND TO COVER PHOTO</p>
           </div>
-          <div className="profilePart flex bg-white pb-12">
-            <img className="ml-4 w-24 mt-4" src={ProfileIcon} alt="profile" />
+          <div className="profilePart flex bg-white pb-2">
+            <img className="ml-4 w-14 mt-4" src={ProfileIcon} alt="profile" />
             <div className="ml-4 mt-4">
-              <h1>Angeles R. Tabrilla</h1>
-              <p>(SAMPLE DESCRIPTION)</p>
+              <h1 className="text-xs">Angeles R. Tabrilla</h1>
+              <p className="text-xs">(SAMPLE DESCRIPTION)</p>
             </div>
           </div>
-          <div className="belowPart bg-white mt-2 pb-12 pt-2 ">
+          <div className="belowPart bg-white mt-1 pb-2 pt-2  ">
             <div className="flex flex-row items-center">
-              <img
-                className="w-24 ml-4 mt-2"
-                src={Connections}
-                alt="Connections"
-              />
-              <p className="ml-8 "> Connections (100)</p>
+              <a href="/">
+                <img
+                  className="w-14 ml-4 mt-2"
+                  src={Connections}
+                  alt="Connections"
+                />
+              </a>
+              <p className="ml-4 text-sm"> Connections (100)</p>
             </div>
             <div className="flex flex-row items-center">
-              <img className="w-24 ml-4 mt-4" src={Events} alt="Events" />
-              <p className="ml-8 "> Events +</p>
+              <a href="/">
+                <img className="w-10 ml-4 mt-4" src={Events} alt="Events" />
+              </a>
+              <p className="ml-8 text-sm "> Events +</p>
             </div>
             <div className="flex flex-row items-center">
-              <img className="w-24 ml-4 mt-4" src={Groups} alt="Groups" />
-              <p className="ml-8 "> Groups</p>
+              <a href="/">
+                <img className="w-12 ml-4 mt-4" src={Groups} alt="Groups" />
+              </a>
+              <p className="ml-6 text-sm"> Groups</p>
             </div>
           </div>
-          <div className=" bg-white mt-2 text-center p-4">
-            <p className="text-base">Try Premium for 3 days free</p>
+          <div className=" text-center bg-white mt-1 p-2">
+            <a href="/">
+              {" "}
+              <p className="text-xs">Try Premium for 3 days free</p>
+            </a>
           </div>
         </div>
       </div>
--- src/Pages/Main/Sidecomponents/RightBar.jsx
+++ src/Pages/Main/Sidecomponents/RightBar.jsx
@@ -3,46 +3,46 @@ import ProfileIcon from "../../../Assets/images/Profile_icon.svg";
 import "./right.css";
 const RightBar = () => {
   return (
-    <div className="flex justify-items-end mt-2 ">
+    <div className="mt-2">
       <div className="holder mr-4 border rounded bg-white ">
-        <div className="titleRecomended mt-4 ml-4 pr-4">
-          <h1 className="font-bold">Highly Recommeded Mentor</h1>
+        <div className="titleRecomended mt-2   text-center">
+          <h1 className="font-bold text-sm">Highly Recommeded Mentor</h1>
         </div>
         <div>
           {/* Profile pic for Recommedation James Mortel*/}
-          <div className="flex  ml-4 mt-4 ">
+          <div className="flex  ml-4 mt-4 pr-2">
             <div className="firstPic">
-              <img className="w-24" src={ProfileIcon} alt="Profile Icon" />
+              <img className="w-12" src={ProfileIcon} alt="Profile Icon" />
             </div>
-            <div className="text-center ml-4">
-              <h1 className="">James Mortel</h1>
-              <p className="text-sm">System Analyst | Web Developer</p>
+            <div className=" ml-4">
+              <h1 className="text-sm font-bold">James Mortel</h1>
+              <p className="text-xs">System Analyst | Web Developer</p>
             </div>
           </div>
-          <div className="ml-32 followButton1">
+          <div className="ml-20  followButton1">
             <button
               type="button"
-              className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+              className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-small rounded-lg text-xs px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
             >
               + Follow
             </button>
           </div>
         </div>
         <div>
-          {/* Profile pic for Recommedation Jeselle Victoria*/}
-          <div className="flex  ml-4 mt-4 ">
+          {/* Profile pic for Recommedation Jeselle Victoria */}
+          <div className="flex  ml-4 mt-4 pr-2">
             <div className="firstPic">
-              <img className="w-24" src={ProfileIcon} alt="Profile Icon" />
+              <img className="w-12" src={ProfileIcon} alt="Profile Icon" />
             </div>
-            <div className="text-center ml-4">
-              <h1 className="">Jeselle Victoria</h1>
-              <p className="text-sm">UI/UX | Photographer</p>
+            <div className="ml-4">
+              <h1 className="text-sm font-bold">Jeselle Victoria</h1>
+              <p className="text-xs">UI/UX | Photographer</p>
             </div>
           </div>
-          <div className="ml-32 followButton1">
+          <div className="ml-20  followButton1">
             <button
               type="button"
-              className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+              className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-small rounded-lg text-xs px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
             >
               + Follow
             </button>
@@ -50,29 +50,23 @@ const RightBar = () => {
         </div>
         <div>
           {/* Profile pic for Recommedation Raphael Isla*/}
-          <div className="flex  ml-4 mt-4 ">
+          <div className="flex  ml-4 mt-4 pr-2">
             <div className="firstPic">
-              <img className="w-24" src={ProfileIcon} alt="Profile Icon" />
+              <img className="w-12" src={ProfileIcon} alt="Profile Icon" />
             </div>
-            <div className="text-center ml-4">
-              <h1 className="">Raphael Isla</h1>
-              <p className="text-sm">Java | Python | C# </p>
+            <div className="ml-4">
+              <h1 className="text-sm font-bold">Raphael Isla</h1>
+              <p className="text-xs">Java | Python | C# </p>
             </div>
           </div>
-          <div className="ml-32 followButton1">
+          <div className="ml-20  followButton1">
             <button
               type="button"
-              className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-base px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
+              className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-small rounded-lg text-xs px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
             >
               + Follow
             </button>
           </div>
-        </div>
-
-        <div className="Recommendations ml-4 pb-8 mt-8 text-neutral-700">
-          <a href="/">
-            <p className="text-base">View all Recommendations &#8594; </p>
-          </a>
         </div>
       </div>
     </div>
