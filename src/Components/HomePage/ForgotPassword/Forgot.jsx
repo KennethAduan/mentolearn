41db3ac3b2994858924930da98388ff98bf2ab9f
Kenneth Aduan <kenneth.aduan10@gmail.com>
2023-01-08 08:29:50 +0800

    ForgotPass

--- src/Components/HomePage/ForgotPassword/Forgot.jsx
+++ src/Components/HomePage/ForgotPassword/Forgot.jsx
@@ -14,16 +14,18 @@ const Forgot = () => {
         </div>
         <div className="flex justify-center mt-36 ">
           {/* BOX HERE */}
-          <form className="holder border rounded  bg-white shadow-md rounded px-12 pt-6 pb-12 mb-4 mt-12 w-96">
+          <form className="holder border rounded  bg-white shadow-md rounded px-12 pt-6 pb-12 mb-4 w-96">
             {/* Title */}
             <div className="Forgot">
-              <h1 className="font-bold">Forgot Passowrd?</h1>
-              <p>Reset Password in two quick steps</p>
+              <h1 className="font-bold text-4xl">Forgot Passowrd?</h1>
+              <p className="text-sm text-zinc-500 mt-2">
+                Reset Password in two quick steps
+              </p>
             </div>
             {/* Input Email */}
             <div className="mt-12">
               <input
-                className="shadow appearance-none border rounded w-full py-8 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-4xl"
+                className="shadow appearance-none border rounded w-full py-8 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-2xl"
                 id="username"
                 type="text"
                 placeholder="Email or Phone"
@@ -33,7 +35,7 @@ const Forgot = () => {
             <div className="ResetButton flex justify-center mt-12">
               <button
                 type="button"
-                class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-4xl px-5 py-2.5 text-center mr-2 mb-2"
+                class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-2xl px-5 py-2.5 text-center mr-2 mb-2"
               >
                 Reset Password
               </button>
