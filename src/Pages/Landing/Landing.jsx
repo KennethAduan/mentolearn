527e64a2236eaa31d857a63e32e8b1a9d5424dba
Kenneth Aduan <kenneth.aduan10@gmail.com>
2023-01-18 12:40:56 +0800

    Landing Page

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import './dist/CSS/animate.css';
+import './dist/CSS/lineIcons.css';
+import './dist/CSS/tailwind.css';
+import './dist/CSS/tiny-slider.css';
+import './dist/CSS/Main.css';
+import './dist/CSS/Style.css';
 import Header from "./Header.jsx";
-import Main from "./Main.jsx";
+import Hero from "./Hero.jsx";
+import Feature from "./Feature.jsx";
+import Service from "./Service.jsx";
+import Team from "./Team.jsx";
+import Footer from "./Footer.jsx";
 const Landing = () => {
   return (
     <div>
       <Header />
-      <Main/>
-      
+      <Hero />
+      <Service />
+      <Feature />
+      <Team />
+      <Footer />
     </div>
   );
 };
